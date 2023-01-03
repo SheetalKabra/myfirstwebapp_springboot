770ccff07b4370d279620959ab27a0b80bba6a84
Sheetal <sheetal@Sheetals-MBP.bbrouter>
2023-01-03 17:54:42 +0530

    spring initializer, say hello controller

---
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,26 @@
+package com.sampleapp.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    //"say-hello" => "Hello! What are you learning today?"
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html");
+        sb.append("<head><title>My first HTML Page</title></head>");
+        sb.append("<body>My first HTML Page with body changed</body>");
+        sb.append("</html");
+        return sb.toString();
+    }
+}
