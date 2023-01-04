699359f14b26411dae96b9c493d246c11693f60d
Sheetal <sheetalkabra4@gmail.com>
2023-01-04 18:16:35 +0530

    get todos from todoService into view

--- src/main/java/com/sampleapp/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,11 +1,15 @@
 package com.sampleapp.springboot.myfirstwebapp.todo;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class TodoService {
     //to get the static list of todos
-    private static List<Todo> todos;
+    private static List<Todo> todos = new ArrayList<>();
     static {
         todos.add(new Todo(1, "in28Minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
         todos.add(new Todo(2, "in28Minutes", "Learn Devops", LocalDate.now().plusYears(2), false));
