2c4ba017e860d6472978fe2eba4b204cd2865da4
Sheetal <sheetalkabra4@gmail.com>
2023-01-04 17:41:13 +0530

    add simple authentication, after successful login redirect it to welcome page

--- src/main/java/com/sampleapp/springboot/myfirstwebapp/LoginController.java
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/LoginController.java
@@ -1,22 +1,36 @@
 package com.sampleapp.springboot.myfirstwebapp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.sampleapp.springboot.myfirstwebapp.login.AuthenticationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    @RequestMapping("login")
-    public String login(@RequestParam String name, ModelMap model){
-        logger.debug("Request param is {}", name);
-        logger.info("Request param is {}", name);
-        logger.info("I want this to print at warn level");
-        model.put("name", name);
+    private AuthenticationService authenticationService;
+
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @RequestMapping(value="login", method = RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping(value="login", method = RequestMethod.POST)
+    public String welcome(@RequestParam String name, @RequestParam String password, ModelMap model){
+        if(authenticationService.autheticate(name, password)){
+            model.put("name", name);
+            model.put("password", password);
+            //add simple Authentication here - lets create a separate class which is responsible for Authentication only
+            //if name = Sheetal, and password = dummy, then only user can enter into Welcome page
+            return "welcome";
+        }
+        model.put("errorMessage", "Invalid Credentials!");
         return "login";
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,12 @@
         <title>Login Page</title>
     <head>
     <body>
-        Welcome to login page, ${name}
+        Welcome to login page!<br>
+        ${errorMessage}
+        <form method="post">
+            Name: <input type="text" name="name">
+            Password: <input type="password" name="password">
+            <input type="submit">
+        </form>
     </body>
 </html>
