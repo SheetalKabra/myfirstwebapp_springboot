90c37a591c045f166983b8ce611ef1325c91854c
Sheetal <sheetalkabra4@gmail.com>
2023-01-04 18:09:12 +0530

    add todo list, for now added static list using TODO service

---
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,19 @@
+package com.sampleapp.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoService {
+    //to get the static list of todos
+    private static List<Todo> todos;
+    static {
+        todos.add(new Todo(1, "in28Minutes", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(2, "in28Minutes", "Learn Devops", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(3, "in28Minutes", "Learn Full Stack Developer", LocalDate.now().plusYears(3), false));
+        todos.add(new Todo(4, "in28Minutes", "Learn Spring Boot", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(5, "in28Minutes", "Learn DSA", LocalDate.now().plusYears(1), false));
+    }
+    public List<Todo> findByUsername(String username){
+        return todos;
+    }
+}
