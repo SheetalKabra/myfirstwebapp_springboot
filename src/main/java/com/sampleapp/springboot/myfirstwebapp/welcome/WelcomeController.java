b64747a5262dd887aa09a02b86a784aa8b67bdee
Sheetal <sheetalkabra4@gmail.com>
2023-01-05 23:39:41 +0530

    preparing for spring security

---
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/welcome/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.sampleapp.springboot.myfirstwebapp.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class WelcomeController {
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+        model.put("name", "in28Minutes");
+        return "welcome";
+    }
+}
