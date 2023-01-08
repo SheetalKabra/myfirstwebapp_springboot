8f20eba93b37096f35b83602c88ab2b9618e5033
Sheetal <sheetalkabra4@gmail.com>
2023-01-08 17:17:24 +0530

    configuring spring security to work h2 console

--- src/main/java/com/sampleapp/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.sampleapp.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -35,6 +39,26 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    //By default Spring security provides 2 features:
+        //1. All URLs are protected
+        //2. A login form is shown for all unauthorized users
+    //To make sure H2 consoles is working,
+        //3. We need to disable CSRF
+        //4. Frames should be available, as by default Spring security does not allows frames
+
+   @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        //show a login form
+        httpSecurity.formLogin(withDefaults());
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions();
+        httpSecurity.headers().frameOptions().sameOrigin();
+        return httpSecurity.build();
     }
 }
 
