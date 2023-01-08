a258b15d6f9cc95ff5febf30a19ea97e1884026a
Sheetal <sheetalkabra4@gmail.com>
2023-01-08 07:49:25 +0530

    setting up a new user for todo application

--- src/main/java/com/sampleapp/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/sampleapp/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -14,14 +14,22 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
     @Bean
     public InMemoryUserDetailsManager createUserDetails() {
+
+        UserDetails userDetails1 = createNewUser("in28Minutes", "dummy");
+        UserDetails userDetails2 = createNewUser("Sheetal", "dummy");
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+
         UserDetails userDetails = User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("in28Minutes")
-                .password("dummy")
+                .username(username)
+                .password(password)
                 .roles("USER", "ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
